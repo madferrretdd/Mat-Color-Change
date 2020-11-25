899076d1e899836ceea43630c67c4d671aedb217
madferrretdd <43165553+madferrretdd@users.noreply.github.com>
2020-11-25 09:24:48 +0000

    First Issue

---
+++ Material Change/Assets/Scripts/Button_Color.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button_Color : MonoBehaviour
+{
+    public int colorButton;
+    private Image myImage;
+    public Material_Change matchange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myImage = GetComponent<Image>();
+        myImage.color = matchange.VehicleColor[colorButton];
+    }
+
+}
---
+++ Material Change/Assets/Scripts/Material_Change.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Material_Change : MonoBehaviour
+{
+    public int colorValue;
+    public Color[] VehicleColor;
+
+    public void OnClickColorPick(int whichColor)
+    {
+        colorValue = whichColor;
+        Material mat = GetComponent<MeshRenderer>().material;
+        mat.SetColor("_Color", VehicleColor[whichColor]);
+    }
+
+
+}
